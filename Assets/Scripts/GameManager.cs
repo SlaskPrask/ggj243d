2e9fc33d43582c5a49dfd2a7087e91ce341de183
Johansson <PJOHAN99@volvocars.com>
2024-01-26 15:40:46 +0100

    Initial push :D

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance {
+        get; private set;
+    }
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else if (instance != this) {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
