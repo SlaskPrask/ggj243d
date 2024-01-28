3833060e001c3b86b8749430602138c974a16551
Johansson <PJOHAN99@volvocars.com>
2024-01-28 14:08:27 +0100

    More sounds

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class AudioManager : MonoBehaviour
 {
@@ -13,10 +14,27 @@ public class AudioManager : MonoBehaviour
     public EventReference postEvent;
     public string[] postTexts;
 
+    [Header("Stuff")]
+    public EventReference pickupSound;
+    public EventReference successSound;
+
     public void PlayOneShot(EventReference eventRef, Vector3 position, int value) {
         var instance = FMODUnity.RuntimeManager.CreateInstance(eventRef);
         instance.set3DAttributes(RuntimeUtils.To3DAttributes(position));
         instance.setParameterByName("RequestType", value);
+        instance.start();
+        instance.release();
+    }
+
+    public void PlayPickup(Vector3 position) {
+        var instance = FMODUnity.RuntimeManager.CreateInstance(pickupSound);
+        instance.set3DAttributes(RuntimeUtils.To3DAttributes(position));
+        instance.start();
+        instance.release();
+    }
+
+    public void PlaySuccess() {
+        var instance = FMODUnity.RuntimeManager.CreateInstance(successSound);
         instance.start();
         instance.release();
     }
